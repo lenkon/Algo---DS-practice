2f435e0e10bf4b852222973c82c8a76fb18e9c5c
K Inoulen Shanongba <inoulenkon@gmail.com>
2023-10-20 10:56:32 +0530

    Add square root test case

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,23 @@
 const assert = require('assert');
 const reverseString = require('../algos/reverse_string.js');
+const squareRoot = require('../algos/square_root.js')
 
-describe('Tests', function () {
+describe('reverseString test', function () {
   it('reverseString() should return a string.', function () {
     assert(typeof reverseString() === 'string');
   });
 
   it('reverseString() should return olleh.', function () {
     assert(reverseString() === 'olleh');
+  });
+});
+
+describe('squareRoot test', function () {
+  it('squareRoot() should return a number.', function () {
+    assert(typeof squareRoot() === 'number');
+  });
+
+  it('squareRoot() should return olleh.', function () {
+    assert(squareRoot() === '2.828');
   });
 });
