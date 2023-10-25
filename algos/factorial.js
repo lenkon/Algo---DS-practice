28ff71cfaa80f6ed5eaf650d4d5cc03451a19132
inoulen inoulen <inoulenkon@gmail.com>
2023-10-25 17:32:17 +0530

    Modify factorial method

--- algos/factorial.js
+++ algos/factorial.js
@@ -1,6 +1,10 @@
 module.exports = function factorial() {
   let n = 10;
+  let ret = getFactorial(n);
+  console.log(ret);
+}
+
+function getFactorial(n) {
   if (n === 0 || n ===1) return 1;
-  else return n * factorial (n-1);
-  console.log(n);
+  else return n * getFactorial (n-1);
 }
