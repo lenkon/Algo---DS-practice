1fb968a109353c573e3acc63fef75f490a431e90
inoulen inoulen <inoulenkon@gmail.com>
2023-12-14 21:27:05 +0530

    Add a palindrome checker function

---
+++ algos/palindrome-checker.js
@@ -0,0 +1,26 @@
+module.exports = function (str) {
+  // write your code here
+  // let input = str.replace(/[\W_]/g, "").toLowerCase();
+  // let inputString = input.split("").reverse().join("");
+  // if (input === inputString) {
+  //   return true;
+  // }
+  // return false;
+
+  let input = str.replace(/[\W_]/g, "").toLowerCase();
+  let inputString = input.split("");
+  let mid = inputString.length / 2;
+  for (let i=0; i<mid; i++) {
+    let j = inputString.length - 1 - i;
+    inputString[i], inputString[j] = inputString[j], inputString[i];
+  }
+  
+  console.log("input: ", input);
+  console.log("inputString: ", inputString);
+  inputString = inputString.reverse().join("");
+  console.log("inputString -- string: ", inputString);
+  if (input === inputString) {
+    return true;
+  }
+  return false;
+}
